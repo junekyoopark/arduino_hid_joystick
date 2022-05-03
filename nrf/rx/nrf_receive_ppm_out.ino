eb5ad65d27a5e07729647473377f6285d34ee97e
junekyoopark <42576592+junekyoopark@users.noreply.github.com>
2022-05-03 22:26:58 +0900

    Add files via upload

---
+++ nrf/rx/nrf_receive_ppm_out.ino
@@ -0,0 +1,114 @@
+
+//nRF INIT PART START
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // CE, CSN
+
+const byte address[6] = "00001";
+//nRF INIT PART END
+
+//PPM INIT PART START
+
+//////////////////////CONFIGURATION///////////////////////////////
+#define chanel_number 8  //set the number of chanels
+#define default_servo_value 1500  //set the default servo value
+#define PPM_FrLen 22500  //set the PPM frame length in microseconds (1ms = 1000µs)
+#define PPM_PulseLen 300  //set the pulse length
+#define onState 1  //set polarity of the pulses: 1 is positive, 0 is negative
+#define sigPin 10  //set PPM signal output pin on the arduino
+//////////////////////////////////////////////////////////////////
+
+
+/*this array holds the servo values for the ppm signal
+ change theese values in your code (usually servo values move between 1000 and 2000)*/
+int ppm[chanel_number];
+
+//PPM INIT PART END
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+
+  //initiallize default ppm values
+  for(int i=0; i<chanel_number; i++){
+    ppm[i]= default_servo_value;
+  }
+
+  pinMode(sigPin, OUTPUT);
+  digitalWrite(sigPin, !onState);  //set the PPM signal pin to the default state (off)
+  
+  cli();
+  TCCR1A = 0; // set entire TCCR1 register to 0
+  TCCR1B = 0;
+  
+  OCR1A = 100;  // compare match register, change this
+  TCCR1B |= (1 << WGM12);  // turn on CTC mode
+  TCCR1B |= (1 << CS11);  // 8 prescaler: 0,5 microseconds at 16mhz
+  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+  sei();
+
+
+}
+
+void loop() {
+  if (radio.available()) {
+    int arr[5];
+    radio.read(&arr, sizeof(arr));
+    //Serial.println((String)arr[0] + arr[1] + arr[2] + arr[3]);
+
+
+    //PPM PART
+    
+    ppm[0] = arr[0];
+    ppm[1] = arr[1];
+    ppm[2] = arr[2];
+    ppm[3] = arr[3];
+
+    //delay(10);
+
+
+    
+  }
+
+
+}
+
+//PPM PART LEAVE ALONE
+ISR(TIMER1_COMPA_vect){  //leave this alone
+  static boolean state = true;
+  
+  TCNT1 = 0;
+  
+  if(state) {  //start pulse
+    digitalWrite(sigPin, onState);
+    OCR1A = PPM_PulseLen * 2;
+    state = false;
+  }
+  else{  //end pulse and calculate when to start the next pulse
+    static byte cur_chan_numb;
+    static unsigned int calc_rest;
+  
+    digitalWrite(sigPin, !onState);
+    state = true;
+
+    if(cur_chan_numb >= chanel_number){
+      cur_chan_numb = 0;
+      calc_rest = calc_rest + PPM_PulseLen;// 
+      OCR1A = (PPM_FrLen - calc_rest) * 2;
+      calc_rest = 0;
+    }
+    else{
+      OCR1A = (ppm[cur_chan_numb] - PPM_PulseLen) * 2;
+      calc_rest = calc_rest + ppm[cur_chan_numb];
+      cur_chan_numb++;
+    }     
+  }
+}
