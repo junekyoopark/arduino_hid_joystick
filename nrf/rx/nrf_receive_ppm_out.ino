aa76dead521f9cda6d4f5b150326503e3da9ea16
junekyoopark <42576592+junekyoopark@users.noreply.github.com>
2022-06-04 12:28:17 +0900

    Update nrf_receive_ppm_out.ino

--- nrf/rx/nrf_receive_ppm_out.ino
+++ nrf/rx/nrf_receive_ppm_out.ino
@@ -13,7 +13,7 @@ const byte address[6] = "00001";
 
 //////////////////////CONFIGURATION///////////////////////////////
 #define chanel_number 8  //set the number of chanels
-#define default_servo_value 1500  //set the default servo value
+#define default_servo_value 900  //set the default servo value
 #define PPM_FrLen 22500  //set the PPM frame length in microseconds (1ms = 1000µs)
 #define PPM_PulseLen 300  //set the pulse length
 #define onState 1  //set polarity of the pulses: 1 is positive, 0 is negative
@@ -62,8 +62,6 @@ void loop() {
   if (radio.available()) {
     int arr[5];
     radio.read(&arr, sizeof(arr));
-    //Serial.println((String)arr[0] + arr[1] + arr[2] + arr[3]);
-
 
     //PPM PART
     
@@ -77,6 +75,15 @@ void loop() {
 
     
   }
+
+  else
+  {
+    for(int i=0; i<chanel_number; i++){
+      ppm[i]= default_servo_value;
+    }
+  }
+
+  //Serial.println((String)ppm[0] + ppm[1] + ppm[2] + ppm[3]);
 
 
 }
