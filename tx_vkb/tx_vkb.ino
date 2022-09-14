7e517d6d9f1443cbdd1c18f1ad7ec5b04fc4231a
junekyoopark <42576592+junekyoopark@users.noreply.github.com>
2022-09-14 14:08:40 +0900

    PPM out added

--- tx_vkb/tx_vkb.ino
+++ tx_vkb/tx_vkb.ino
@@ -1,15 +1,10 @@
-/*Joystick Report Parser */
+/* Simplified Logitech Extreme 3D Pro Joystick Report Parser */
 
 #include <usbhid.h>
 #include <hiduniversal.h>
 #include <usbhub.h>
 
 #include "vkb.h"
-
-//APC PART
-#include <SoftwareSerial.h>
-SoftwareSerial APC(3, 4); // RX and TX
-//
 
 
 // Satisfy the IDE, which needs to see the include statment in the ino too.
@@ -24,12 +19,55 @@ HIDUniversal                                    Hid(&Usb);
 JoystickEvents                                  JoyEvents;
 JoystickReportParser                            Joy(&JoyEvents);
 
+
+
+
+//PPM INIT PART START
+
+//////////////////////CONFIGURATION///////////////////////////////
+#define chanel_number 6  //set the number of chanels
+#define default_servo_value 1100  //set the default servo value
+#define PPM_FrLen 20000  //set the PPM frame length in microseconds (1ms = 1000µs)
+#define PPM_PulseLen 400  //set the pulse length //300//
+#define onState 0  //set polarity of the pulses: 1 is positive, 0 is negative
+#define sigPin 8  //set PPM signal output pin on the arduino
+//////////////////////////////////////////////////////////////////
+
+
+/*this array holds the servo values for the ppm signal
+ change theese values in your code (usually servo values move between 1000 and 2000)*/
+int ppm[chanel_number];
+
+//PPM INIT PART END
+
+
+
+
 void setup()
 {
-  ///APC PART
-  APC.begin(9600);    // start serial to APC
-  ////
   Serial.begin( 115200 );
+
+
+  //initiallize default ppm values
+  for(int i=0; i<chanel_number; i++){
+    ppm[i]= default_servo_value;
+  }
+
+  pinMode(sigPin, OUTPUT);
+  digitalWrite(sigPin, !onState);  //set the PPM signal pin to the default state (off)
+  
+  cli();
+  TCCR1A = 0; // set entire TCCR1 register to 0
+  TCCR1B = 0;
+  
+  OCR1A = 100;  // compare match register, change this
+  TCCR1B |= (1 << WGM12);  // turn on CTC mode
+  TCCR1B |= (1 << CS11);  // 8 prescaler: 0,5 microseconds at 16mhz
+  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+  sei();
+
+
+  
 #if !defined(__MIPSEL__)
   while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
 #endif
@@ -42,73 +80,69 @@ void setup()
 
   if (!Hid.SetReportParser(0, &Joy))
       ErrorMessage<uint8_t>(PSTR("SetReportParser"), 1  );
+      
 }
 
 
-//these were initialized to save bandwidth
-int old_arr[5] = { 0 }; //initializing array in loop()
-int arr[5] = { 0 }; //initializing array in loop()
 
-int i; //initializing counter for going through elements in array
-int change = 0; //initializing variable. Checks whether there is a change in input or not. 
-//
+int arr[6]; //initializing array in loop()
 
 void loop()
 {
     Usb.Task();
-    extern uint16_t x, y, rudder, throttle;
-    arr[0] = x;
-    arr[1] = y;
-    arr[2] = rudder;
-    arr[3] = throttle;
-    arr[4] = 0;
- 
-    for (i=0; i<=4; i++)
-    {  
-      if (old_arr[i] != arr[i])
-      {
-        change = 1;
-      }
-      old_arr[i] = arr[i];
-    }
+    //extern uint16_t x, y, rudder, throttle;
+    //ch1 roll - x
+    //ch2 pitch - y
+    //ch3 throttle
+    //ch4 rudder
+    extern int x, y, rudder, throttle;
 
-    if (change == 1)
-    {
-      Serial.println((String)arr[0] + "///" + arr[1] + "///" + arr[2] + "///" + arr[3] + "///" + arr[4]);
-      APC.println((String)arr[0] + "///" + arr[1] + "///" + arr[2] + "///" + arr[3] + "///" + arr[4]);
+    //normalize values. PPM range is 1000-2000, USB values are x, y = 0~4095, rudder and throttle 0~2048, so 
+    arr[0] = x * 0.244140625 + 1000;
+    arr[1] = (-y+4095) * 0.244140625 + 1000;
+    //since throttle is inversed
+    arr[2] = (-throttle+2048) * 0.48828125 + 1000;
+    arr[3] = rudder * 0.48828125 + 1000;
+
+  //PPM PART
+    
+    ppm[0] = arr[0];
+    ppm[1] = arr[1];
+    ppm[2] = arr[2];
+    ppm[3] = arr[3];
+
+    
+
+}
+
+//PPM PART LEAVE ALONE
+ISR(TIMER1_COMPA_vect){  //leave this alone
+  static boolean state = true;
+  
+  TCNT1 = 0;
+  
+  if(state) {  //start pulse
+    digitalWrite(sigPin, onState);
+    OCR1A = PPM_PulseLen * 2;
+    state = false;
+  }
+  else{  //end pulse and calculate when to start the next pulse
+    static byte cur_chan_numb;
+    static unsigned int calc_rest;
+  
+    digitalWrite(sigPin, !onState);
+    state = true;
+
+    if(cur_chan_numb >= chanel_number){
+      cur_chan_numb = 0;
+      calc_rest = calc_rest + PPM_PulseLen;// 
+      OCR1A = (PPM_FrLen - calc_rest) * 2;
+      calc_rest = 0;
     }
-    
-    i = 0;
-    change = 0;
-    
-//    Serial.print("X: ");
-//    Serial.print(x);
-//    Serial.print(" ");
-//
-//    APC.println((String)x + "///" + y + "///" + rudder + "///" + throttle);
-//    
-//  
-//    Serial.print("Y: ");
-//    Serial.print(y);
-//    Serial.print(" ");
-////
-////    APC.print("///");
-////    APC.print(y);
-//  
-//    Serial.print("Rudder: ");
-//    Serial.print(rudder);
-//    Serial.print(" ");
-////
-////    APC.print("///");
-////    APC.print(rudder);
-//
-//    Serial.print("Throttle: ");
-//    Serial.print(throttle);
-//    Serial.print(" ");
-////    
-////    APC.print("///");
-////    APC.print(throttle);
-//
-//    
-//    Serial.println("");
+    else{
+      OCR1A = (ppm[cur_chan_numb] - PPM_PulseLen) * 2;
+      calc_rest = calc_rest + ppm[cur_chan_numb];
+      cur_chan_numb++;
+    }     
+  }
 }
