31f6e44ff45f9cd13588567ce777b72b27e9fcb4
junekyoopark <42576592+junekyoopark@users.noreply.github.com>
2022-10-31 16:00:57 +0900

    Update vkb.h struct with switches support.
    Added trigger, hat switch, and function buttons support.

--- tx_vkb/vkb.h
+++ tx_vkb/vkb.h
@@ -12,7 +12,18 @@ struct GamePadEventData
       uint32_t x : 16;
       uint32_t y : 16;
       uint32_t rudder : 16;
-      uint32_t throttle : 11;      
+      uint32_t throttle : 11;
+      uint32_t x_2 : 16;
+      uint32_t y_2 : 16;
+      
+      uint32_t filler1 : 8;
+      uint32_t filler2 : 8;
+      uint32_t filler3 : 8;
+      uint32_t filler4 : 8;
+      
+      uint32_t hatswitch : 16;
+      uint32_t toptrigger : 8;
+      uint32_t funcbuttons : 8;
     };
   };
 };
